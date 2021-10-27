55e7756bb43566e0b04300dd3642909982c40353
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2021-10-27 13:20:51 +0200

    1. beadandó végleges

--- 1.bead/Task1.java
+++ 1.bead/Task1.java
@@ -5,10 +5,11 @@ public class Task1 {
 
   /* Create new sorted array by merging 2 smaller sorted arrays */
   private static int[] merge(int[] arr1, int[] arr2) {
-    // TODO: merge sorted arrays 'arr1' and 'arr2'
+
     int[] result = new int[arr1.length + arr2.length];
-    int i = 0;
-    int j = 0;
+
+    int i = 0; // iterates over arr1
+    int j = 0; // iterates over arr2
 
     while (i < arr1.length && j < arr2.length) {
       if (arr1[i] > arr2[j])
@@ -30,7 +31,7 @@ public class Task1 {
 
   /* Creates an array of arrays by slicing a bigger array into smaller chunks */
   private static int[][] slice(int[] arr, int k) {
-    // TODO: cut 'arr' into 'k' smaller arrays
+
     int newLen = arr.length / k;
     int rem = arr.length % k;
     int[][] result = new int[k][];
@@ -38,13 +39,11 @@ public class Task1 {
     int start = 0;
     int end;
     for (int i = 0; i < k; i++) {
-      end = start + newLen;
-      if (rem-- > 0)
-        end++;
-      
+      end = start + newLen + (rem-- > 0 ? 1 : 0);
       result[i] = Arrays.copyOfRange(arr, start, end);
       start = end;
     }
+
     return result;
   }
 
@@ -52,16 +51,13 @@ public class Task1 {
   public static int[] sort(int[] array) {
 
     /* Initialize variables */
-    // TODO: check available processors and create necessary variables
     int procs = Runtime.getRuntime().availableProcessors();
     Thread[] threads = new Thread[procs];
 
     /* Turn initial array into array of smaller arrays */
-    // TODO: use 'slice()' method to cut 'array' into smaller bits
     int[][] split = slice(array, procs);
 
     /* parralelized sort on the smaller arrays */
-    // TODO: use multiple threads to sort smaller arrays (Arrays.sort())
     for (int i = 0; i < procs; i++) {
       int[] arr = split[i];
       threads[i] = new Thread(() -> Arrays.sort(arr));
@@ -75,22 +71,17 @@ public class Task1 {
       try {
         thread.join();
       } catch (InterruptedException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
       }
     }
 
     /* Merge sorted smaller arrays into a singular larger one */
-    // TODO: merge into one big array using 'merge()' multiple times
-    // create an empty array called 'sorted' and in a for cycle use
-    // 'merge(sorted, arr2d[i])' where arr2d is an array of sorted arrays
     int[] sorted = split[0];
     for (int i = 1; i < procs; i++) {
       sorted = merge(sorted, split[i]);
     }
 
     /* Return fully sorted array */
-    // TODO: return the sorted array and delete all lines starting with '//'
     return sorted;
   }
 }
--- 1.bead/Task2.java
+++ 1.bead/Task2.java
@@ -5,19 +5,11 @@ public class Task2 {
 
   /* Create new sorted array by merging 2 smaller sorted arrays */
   private static void merge(int[] src, int[] dst, int idx1, int idx2, int end) {
-    // TODO: 'src' is sorted between [idx1,idx2) and [idx2,end)
-    // copy both to 'dst' in a way that [idx1,end) is sorted for 'dst'
-    // Note: 'idx1' is the starting point of the 1st array
-    // 'idx2' is the starting point of the 2nd array
-    // 'end' is the end of the 2nd array (exclusive)
-    // There are no elements between the first and second arrays
-    // 'src' is the source, this is where the 2 smaller sorted arrays are
-    // 'dst' is the destination, this is where you have to move data
-    // Merge the 2 smaller arrays using the same methodology as in 'Task1'
 
-    int i = idx1;
-    int j = idx2;
-    int k = idx1;
+    int i = idx1; // iterates over src 1st half
+    int j = idx2; // iterates over src 2nd half
+    int k = idx1; // iterates over dst
+
     while (i < idx2 && j < end) {
       if (src[i] > src[j])
         dst[k++] = src[j++];
@@ -38,26 +30,21 @@ public class Task2 {
   public static void kernel(int[] src, int[] dst, int start, int end, int depth) {
 
     /* Single thread sort if bottom has been reached */
-    // TODO: simply sort the array using 'Arrays.sort()' if depth is zero.
     if (depth == 0) {
       Arrays.sort(src, start, end);
       return;
     }
 
     /* Otherwise split task into two recursively */
-    // TODO: summon another thread and recursively sort left and right half
-    // NOTE: don't forget to make recursive call with 'depth-1'
     int middle = (start + end) / 2;
     Thread fisrt = new Thread(() -> kernel(src, dst, start, middle, depth - 1));
-    Thread second = new Thread(() -> kernel(src, dst, middle, end, depth - 1));
     fisrt.start();
-    second.start();
+
+    kernel(src, dst, middle, end, depth - 1);
 
     try {
       fisrt.join();
-      second.join();
     } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
@@ -71,7 +58,6 @@ public class Task2 {
   public static int[] sort(int[] array) {
 
     /* Initialize variables */
-    // TODO: Create 'src' and 'dst' arrays
     int[] src = array.clone();
     int[] dst = new int[array.length];
 
@@ -83,14 +69,8 @@ public class Task2 {
     int optDepth = Math.max(0, Math.min(procDepth, arrDepth));
 
     /* Launch recursive core */
-    // TODO: launch kernel, call with 'optDepth' (not 'optDepth-1')
     kernel(src, dst, 0, array.length, optDepth);
 
-    // TODO: return src or dst depending on the parity of the used depth
-    // TODO: delete all lines starting with '//'
-    if (optDepth % 2 == 0)
-      return src;
-    else
-      return dst;
+    return optDepth % 2 == 0 ? src : dst;
   }
 }
